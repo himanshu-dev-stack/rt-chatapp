51a0c3b37a92704aaca0eb4113451a1096d2caca
Himanshu <codemarketing.offical@gmail.com>
2025-12-27 13:46:58 +0530

    Update axios instance baseURL for production
    Set baseURL to a fixed API endpoint for production.

--- chatapp/frontend/src/lib/axios.js
+++ chatapp/frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://rt-chatapp.vercel.app/api" : "/api",
+  baseURL: "https://rt-chatapp.vercel.app/api",
   withCredentials: true,
 });
