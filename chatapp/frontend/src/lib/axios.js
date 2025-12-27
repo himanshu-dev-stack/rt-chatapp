ddf543c27966d8b8c7adec74bafa0cc2945afa33
Himanshu <codemarketing.offical@gmail.com>
2025-12-27 14:19:13 +0530

    Update axios.js

--- chatapp/frontend/src/lib/axios.js
+++ chatapp/frontend/src/lib/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-const API_URL = import.meta.env.VITE_API_URL;
 
 export const axiosInstance = axios.create({
-  baseURL: API_URL,
+  baseURL:  "https://rt-chatapp.vercel.app/api",
   withCredentials: true,
 });
